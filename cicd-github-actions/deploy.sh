33ab75573e9f701d1e96742ce674ad55a522efd5
Uche John <juvobest@gmail.com>
2026-01-03 08:00:09 +0100

    Add CI/CD pipeline with GitHub Actions

---
+++ cicd-github-actions/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+APP_DIR=/home/devopsuser/apps/portfolio
+
+if [ ! -d "$APP_DIR/.git" ]; then
+  git clone https://github.com/juvobest/devops-portfolio.git $APP_DIR
+else
+  cd $APP_DIR
+  git pull origin main
+fi
+
+cd $APP_DIR/docker-compose-app
+docker compose down
+docker compose up -d
